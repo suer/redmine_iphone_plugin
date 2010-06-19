3885a3b6572ac6fef9e1203a58c98f02eec1b4c3
xdite <xdite@pixnet.tw>
2010-06-19 18:56:53 +0800

    把 issues 改到 project 下

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ ActionController::Routing::Routes.draw do |map|
   map.iphone '/iphone', :controller => "iphone"
   
   map.namespace :iphone do |iphone|
-    iphone.resources :issues
-    iphone.resources :projects
+    #iphone.resources :issues
+    iphone.resources :projects do |project|
+      project.resources :issues
+    end
     iphone.resource  :timelog, :controller => "timelog", :collection => { :details => :get }  
   end
 
